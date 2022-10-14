d5e909eac26488df0d9194c10b924cfe6da9f71f
Lilyan <lilyan0800@gmail.com>
2022-10-14 11:17:06 +0700

    Belajar Dasar Pemrograman Web

---
+++ assets/js/popup.js
@@ -0,0 +1,28 @@
+const boxContainer = document.querySelector(".box-container");
+const popup = document.querySelector(".popup-box");
+const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+boxContainer.addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() == "button") {
+    const item = event.target.parentElement;
+    const h3 = item.querySelector("h3").innerHTML;
+    const readMoreCont = item.querySelector(".read-more-content").innerHTML;
+    popup.querySelector("h3").innerHTML = h3;
+    popup.querySelector(".popup-body").innerHTML = readMoreCont;
+    popupBox();
+  }
+});
+
+popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
+
+popup.addEventListener("click", function (event) {
+  if (event.target == popup) {
+    popupBox();
+  }
+});
+
+function popupBox() {
+  popup.classList.toggle("open");
+}
